fd196e02dec043476397cbc1bfa341cc38ff7364
caleb bourg <calebbourg2@gmail.com>
2016-11-23 15:38:07 -0800

    specs pass

--- cart.rb
+++ cart.rb
@@ -1,28 +1,37 @@
 class Item
-  attr_reader :name, :price
+  attr_reader :name, :price, :taxable
 
-  def initialize(name, price)
+  def initialize(name, price, taxable = true)
     @name = name
     @price = price
+    @taxable = taxable
   end
 end
 
 class Cart
+
+  CURRENT_TAX = 0.10
+
   def initialize
     @items = []
   end
 
   def total
-    @items.map(&:price).inject(&:+)
+    subtotal + tax 
   end
 
   def subtotal
+    @items.map(&:price).inject(&:+)
   end
 
   def tax
+    taxables = []
+    @items.each { |item| taxables << item if item.taxable }
+    taxables.empty? ? 0 : taxables.map(&:price).inject(&:+)*CURRENT_TAX
   end
 
   def add_item(item)
     @items << item
   end
 end
+
--- cart_spec.rb
+++ cart_spec.rb
@@ -1,26 +1,34 @@
 require './cart'
 
 RSpec.describe Cart do
-  let(:item) { Item.new('Carrot', 100) }
-
-  describe '#subtotal' do
-    it 'returns the total amount from the cart without tax' do
-      cart = Cart.new
-      cart.add_item(item)
-      cart.add_item(item)
-      expect(cart.total).to eq(200)
-    end
-  end
+  let(:taxable_item) { Item.new('Chair', 100) }
+  let(:non_taxable_item) { Item.new('Carrot', 100, false) }
 
   # 1) Implement the code that passes this test, assume tax rate of 10%
   describe '#total' do
     it 'returns the total amount from the cart with tax' do
       cart = Cart.new
-      cart.add_item(item)
-      cart.add_item(item)
-      expect(cart.subtotal).to eq(220)
+      cart.add_item(taxable_item)
+      cart.add_item(non_taxable_item)
+      expect(cart.total).to eq(210)
     end
   end
 
-  # 2) Implement the idea that some items are produce and do not incur tax and write the tests that relate to this behavior. Feel free to change anything, add methods, delete methods, add classes, delete classes.
+  describe '#subtotal' do
+    it 'returns the total amount from cart without tax' do
+      cart = Cart.new
+      cart.add_item(taxable_item)
+      cart.add_item(non_taxable_item)
+      expect(cart.subtotal).to eq(200)
+    end
+  end
+
+  describe '#tax' do
+    it 'returns tax amount for only taxable items' do
+      cart = Cart.new
+      cart.add_item(taxable_item)
+      cart.add_item(non_taxable_item)
+      expect(cart.tax).to eq(10)
+    end
+  end
 end
